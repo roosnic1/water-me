9bca0bfd2cbb0e1088050475a4b31a016cae127a
Nicolas Roos <nicolas.k.roos@gmail.com>
2021-05-06 15:12:43 +0200

    feat(app): added reactfire for firebase handling

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import 'firebase/firestore'
+import { FirebaseAppProvider, useFirestoreDocData, useFirestore, SuspenseWithPerf } from 'reactfire';
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
+};
+
+function HelloMessage() {
+    const helloRef = useFirestore().collection('hello').doc('test')
+
+    const {data} = useFirestoreDocData(helloRef)
+
+    // @ts-ignore
+    return <p>{data.text}</p>
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <FirebaseAppProvider firebaseConfig={firebaseConfig} suspense={true}>
+          <SuspenseWithPerf fallback={'loading burrito status...'} traceId={'load-test-status'}>
+            <HelloMessage />
+          </SuspenseWithPerf>
+      </FirebaseAppProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
