e3409bfbdf89435a1cda6f65f432f3f91716bf35
Nicolas Roos <nicolas.k.roos@gmail.com>
2021-05-09 15:48:56 +0200

    feat(app): added secrets to github deploy

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,7 @@ const firebaseConfig = {
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
     databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAE_BUCKET,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
